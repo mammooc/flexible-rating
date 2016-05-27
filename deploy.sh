b03bc0657197f78f9e20d7345d5c3e51e3b3a088
Philipp Fischbeck <philipp.fischbeck@student.hpi.uni-potsdam.de>
2016-05-27 11:51:45 +0200

    Try to fix auto-deployment

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,60 @@
-# git clone the Polymer tools repository somewhere outside of your 
-# element project
-git clone git://github.com/Polymer/tools.git
-
+# @license
+# Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+# This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+# The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+# The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+# Code distributed by Google as part of the polymer project is also
+# subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+#
 # Create a temporary directory for publishing your element and cd into it
 mkdir temp && cd temp
 
-# Run the gp.sh script. This will allow you to push a demo-friendly
-# version of your page and its dependencies to a GitHub pages branch
-# of your repository (gh-pages). Below, we pass in a GitHub username
-# and the repo name for our element
-../tools/bin/gp.sh mammooc flexible-rating
+
+# This script pushes a demo-friendly version of your element and its
+# dependencies to gh-pages.
+
+# Run in a clean directory passing in a GitHub org and repo name
+org=mammooc
+repo=flexible-rating
+branch=${3:-"master"} # default to master when branch isn't specified
+
+# make folder (same as input, no checking!)
+mkdir $repo
+git clone "https://${GH_TOKEN}@github.com:$org/$repo.git" --single-branch
+
+# switch to gh-pages branch
+pushd $repo >/dev/null
+git checkout --orphan gh-pages
+
+# remove all content
+git rm -rf -q .
+
+# use bower to install runtime deployment
+bower cache clean $repo # ensure we're getting the latest from the desired branch.
+git show ${branch}:bower.json > bower.json
+echo "{
+  \"directory\": \"components\"
+}
+" > .bowerrc
+bower install
+bower install $org/$repo#$branch
+git checkout ${branch} -- demo
+rm -rf components/$repo/demo
+mv demo components/$repo/
+
+# redirect by default to the component folder
+echo "<META http-equiv="refresh" content=\"0;URL=components/$repo/\">" >index.html
+
+# config
+git config --global user.email "mammooc@outlook.com"
+git config --global user.name "mammooc Bot"
+
+# send it all to github
+git add -A .
+git commit -am 'Deploy to Github Pages'
+git push -u origin gh-pages --force
+
+popd >/dev/null
 
 # Finally, clean-up your temporary directory as you no longer require it
 cd ..
