731dce89155c8fbe4e85fed5de25ca3dbeca2b3f
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-09-23 19:04:54 +0200

    Update entrypoint in json files

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 npm --no-git-tag-version version $1
+git add package.json package-lock.json
+git commit -m $1
 git push
 ./utils/github-pages.sh mammooc flexible-rating
 rm -rf ./flexible-rating
