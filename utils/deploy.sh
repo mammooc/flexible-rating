557c27940a39c91163872b443ebf9137c09a79ea
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2018-07-27 01:27:47 +0200

    Update deploy script to include `npm publish` command

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -3,5 +3,6 @@ npm --no-git-tag-version version $1
 git add package.json package-lock.json
 git commit -m $1
 git push
+npm publish
 ./utils/github-pages.sh mammooc flexible-rating
 rm -rf ./flexible-rating
